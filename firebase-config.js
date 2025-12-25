157813dd86850cd3720d9c92dce9b0abed58132a
tul-hub <77601909+tul-hub@users.noreply.github.com>
2025-12-25 21:25:22 +0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAU9YdxgNzUOn5D5ouT8EV3dSVMIeYmIis",
+    authDomain: "spicy-45a5c.firebaseapp.com",
+    projectId: "spicy-45a5c",
+    storageBucket: "spicy-45a5c.firebasestorage.app",
+    messagingSenderId: "821744378040",
+    appId: "1:821744378040:web:ae3310ec4efe3224b6d44d"
+};
